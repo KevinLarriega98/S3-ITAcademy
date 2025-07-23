11fbfe1821e6dc3b87a0842a62708b294927ae80
KevinLarriega98 <kevinlarriega@gmail.com>
2025-07-23 13:04:15 +0200

    Exercise 5 completed

--- src/films.js
+++ src/films.js
@@ -39,8 +39,18 @@ function orderAlphabetically(array) {
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(array) {
+  const copiedArray = [...array]
 
+  copiedArray.sort((a, b) => {
+    if (a.year !== b.year) {
+      return a.year - b.year
+    } else {
+      return a.title.localeCompare(b.title)
+    }
+  })
+
+  return copiedArray
 }
 
 // Exercise 6: Calculate the average of the movies in a category
